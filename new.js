0f46f76150432ac9112d93c3747af4c92819c3ba
Assonkeng Ortus <oassonkeng@gmail.com>
2026-01-19 04:25:31 +0200

    web pages files

---
+++ new.js
@@ -0,0 +1,16 @@
+// Simple JavaScript program
+
+// Declare variables
+let name = "Ortus";
+let age = 20;
+
+// Perform a calculation
+let nextYearAge = age + 1;
+
+// Display output
+console.log("Name:", name);
+console.log("Current Age:", age);
+console.log("Age next year:", nextYearAge);
+
+// Display alert in browser
+alert("Hello " + name + "! Next year you will be " + nextYearAge + " years old.");
